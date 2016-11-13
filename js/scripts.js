ceaf4f6cbd01c4d05d88139d888b0db056075438
Sandra <sandra@rcss-MacBook-Pro.local>
2016-11-13 11:50:47 +0100

    new js change case animal

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// plik scripts.js
+
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.',
+	animalName = 'Zielone słonie',
+	animalNameUpperCased = animalName.toUpperCase(),
+	textChangedAnimal = text.replace('Papugi', animalNameUpperCased);
+
+console.log(textChangedAnimal.slice(1, textChangedAnimal.length/2));
